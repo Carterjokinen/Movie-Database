67f5ddd35ab54ba1d71c84364b8bdbb4c65db5f4
Carterjokinen <carter.jokinen@gmail.com>
2023-02-08 20:17:04 -0600

    migrations

--- src/db/migrations/20230207204739_createCriticsTable.js
+++ src/db/migrations/20230207204739_createCriticsTable.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("critics", (table) => {
+      table.increments("critic_id").primary(); //set the critic_id as the primary
+      table.string("preferred_name");
+      table.string("surname");
+      table.string("organization_name");
+      table.timestamps(true, true);
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function (knex) {
+    return knex.schema.dropTable("critics");
+  };
   
-};
