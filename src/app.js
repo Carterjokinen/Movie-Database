340f054e2df154387da17d39feb8f30f87316bae
Carterjokinen <carter.jokinen@gmail.com>
2023-02-09 00:24:07 -0600

    Updating app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,20 @@ const notFound = require("./errors/notFound");
 app.use(cors());
 app.use(express.json());
 
+//----Routes----//
+
+const router = express.Router();
+
+router.get("/", cors(), (req, res) => {
+    res.json({ message: "Welcome! You can access the data using these routes: /movies, /movies/:movieId, /theaters, movies/:movieId/reviews, /movies/:movieId/reviews/:reviewId" })
+})
+
+app.use("/", router);
+
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
 //----Error handlers----//
 app.use(notFound);
 app.use(errorHandler);
